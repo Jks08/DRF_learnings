a5b84cd2509e00afdd7fd5a9b4a29d8da0dfcfee
Jishnu Kumar Srivastava <jisheras31@gmail.com>
2023-03-02 16:59:31 +0530

    broke validations into functions

--- project1/project1/settings.py
+++ project1/project1/settings.py
@@ -138,6 +138,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MAX_ACCOUNTS_PER_CUSTOMER = 4
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
