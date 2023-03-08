5f6bc676af80bffa3017651015825d0d9e64c915
Jishnu Kumar Srivastava <jisheras31@gmail.com>
2023-03-08 14:11:00 +0530

    jsons folder and base mngt cmd created

--- project1/kbapp/mutualfunds/management/commands/dataload.py
+++ project1/kbapp/mutualfunds/management/commands/dataload.py
@@ -4,4 +4,11 @@ from django.core.management.base import BaseCommand
 from kbapp.models import AMC
 
 class Command(BaseCommand):
-    pass
+    help = 'Load AMC data from json file'
+
+    def load_data_to_amc(self, data):
+        pass
+
+    def handle(self, *args, **kwargs):
+        file = open('project1/jsons/AMC.json', 'r')
+        return "Blank"
