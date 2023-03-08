ba6c23f6d612d309568690755664f8b783323302
Jishnu Kumar Srivastava <jisheras31@gmail.com>
2023-03-08 17:04:50 +0530

    cleaning up

--- project1/kbapp/mutualfunds/admin.py
+++ project1/kbapp/mutualfunds/admin.py
@@ -5,9 +5,5 @@ from kbapp.models import AMC
 
 class AMCAdmin(admin.ModelAdmin):
     model = AMC
-    # list_display = ['amc_id','name','rta_amc_code','is_being_sold','is_active']
-    # print all column names from AMC model without using model._meta.fields
-    # for field in AMC._meta.fields:
-        # list_display.append(field.name)
     list_display = [field.name for field in AMC._meta.fields]
 admin.site.register(AMC, AMCAdmin)
