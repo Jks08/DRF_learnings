15da7b8f5a3a2fe67f30e5e6878b827d418856a5
Jishnu Kumar Srivastava <jisheras31@gmail.com>
2023-03-08 16:32:22 +0530

    Made admin for AMC more friendly

--- project1/kbapp/mutualfunds/admin.py
+++ project1/kbapp/mutualfunds/admin.py
@@ -3,4 +3,8 @@ from kbapp.models import AMC
 
 # Register your models here.
 
-admin.site.register(AMC)
+class AMCAdmin(admin.ModelAdmin):
+    model = AMC
+    list_display = ['amc_id','name','rta_amc_code','is_being_sold','is_active']
+
+admin.site.register(AMC, AMCAdmin)
