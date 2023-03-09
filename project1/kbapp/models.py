a8e8ee24bc7f8da0262b63183b3e2813cb89a323
Jishnu Kumar Srivastava <jisheras31@gmail.com>
2023-03-09 15:57:56 +0530

    added fields to AMCFund model

--- project1/kbapp/models.py
+++ project1/kbapp/models.py
@@ -44,12 +44,33 @@ class AMC(BaseField):
         db_table = 'kbapp_amc'
 
 class AMCFund(BaseField):
+    amcfund_id = models.AutoField(primary_key=True)
     rta_fund_code = models.CharField(max_length=10, unique=True, default="NULL")
     name = models.CharField(max_length=255)
     fund_type = models.CharField(max_length=100, null=True, blank=True)
     fund_category = models.CharField(max_length=100, null=True, blank=True)
     fund_sub_type = models.CharField(max_length=100, null=True, blank=True)
     risk_factor = models.CharField(max_length=20, null=True, blank=True)
+    description = models.TextField(max_length=1024, default='N.A', blank=True)
+    assets_under_management = models.FloatField(default=0.0, blank=True, null=True)
+    assets_under_management_date = models.DateField(null=True, blank=True)
+    launch_date = models.DateField(null=True, blank=True)
+    is_being_sold = models.BooleanField(null=True, default=True)
+    direct_advantage_savings = models.FloatField(default=0.0, max_length=20, null=True, blank=True)
+    smart_savings = models.FloatField(default=0.0, max_length=20, null=True, blank=True)
+    exit_load_codes = models.CharField(null=True, max_length=128, blank=True)
+    fund_class = models.CharField(max_length=64, null=True, blank=True)
+    fund_type_id = models.SmallIntegerField(null=True, blank=True)
+    scheme_information_document_url = models.URLField(blank=True, null=True)
+    amcfund_rating = models.PositiveIntegerField(default=0, null=True, blank=True)
+    direct_plan_expense_ratio_in_perc = models.FloatField(default=0.0, null=True, blank=True)
+    regular_plan_expense_ratio_in_perc = models.FloatField(null=True, blank=True)
+    expense_ratio_as_on_date = models.DateField(null=True, blank=True)
+    fund_manager = models.CharField(max_length=64, blank=True, null=True)
+    fund_objective = models.TextField(max_length=512, blank=True, null=True)
+    fund_manager_since = models.DateField(null=True, blank=True)
+    turn_over_ratio = models.FloatField(default=0.0, null=True, blank=True)
+    turn_over_ratio_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return str(self.name) + str(self.rta_fund_code)
