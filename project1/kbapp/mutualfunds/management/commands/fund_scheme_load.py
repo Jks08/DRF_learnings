90507289599ea6db503644dc43983995fcf2e3fb
Jishnu Kumar Srivastava <jisheras31@gmail.com>
2023-03-10 10:43:20 +0530

    implemented api data fetch and write to AMCFund
    The management command fund_scheme_load.py is implemented to fetch the
    data from the api and write to the AMCFund table. The data is fetched
    from the api and only relevant fields are written to the AMCFund table.

---
+++ project1/kbapp/mutualfunds/management/commands/fund_scheme_load.py
@@ -0,0 +1,69 @@
+from django.core.management.base import BaseCommand
+import requests
+from kbapp.models import AMCFund
+import datetime
+
+
+def get_data_from_api(url):
+    response = requests.get(url)
+    data = response.json()
+    return data
+
+def fund_payload():
+    payload = {
+        "rta_fund_code": "scheme",
+        "name": "schdesc",
+        "fund_type": "category",
+        "fund_category": "category",
+        "fund_sub_type": "subcategory",
+        "risk_factor": "risktype",
+    }
+    return payload
+
+def store_fund_data(data):
+    # Our data coming from the api looks like this:
+    # {
+    # "statusCode": "0",
+    # "message": "success",
+    # "data": {
+    #     "fund": "189",
+    #     "fundname": "Bajaj MUTUAL FUND",
+    #     "funds": [
+    #         {
+    #             "scheme": "IS",
+    #             "schdesc": "Bajaj Regular Saver Fund",
+    #             "category": "DEBT FUND",
+    #             "subcategory": "DEBT",
+    #             "risktype": "MODERATELY HIGH",
+    #             "schemes":[....]
+    #         },
+    #         {
+    #             "scheme": "IS",
+
+    # and so on
+    # We need to iterate over it and store data under the funds list from "scheme"
+    # to "risktype" in the AMCFund model
+    payload = fund_payload()
+    for fund in data['data']['funds']:
+        # print(f"{fund['scheme']} | {fund['schdesc']} | {fund['category']} | {fund['subcategory']} | {fund['risktype']}")
+        obj = AMCFund.objects.filter(rta_fund_code=fund['scheme']).first()
+        if obj:
+            for key, value in payload.items():
+                setattr(obj, key, fund[value])
+            obj.modified_by = "Admin User"
+            obj.modified = datetime.datetime.now()
+            obj.save()
+        else:
+            obj_new = AMCFund(**payload)
+            for key, value in payload.items():
+                setattr(obj_new, key, fund[value])
+            obj_new.created_by = "Admin User"
+            obj_new.created = datetime.datetime.now()
+            obj_new.save()
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        url = "https://clientwebsitesuat3.kfintech.com/bajaj/api/v1/masterData/getSchemes"
+        data = get_data_from_api(url)
+        print(store_fund_data(data))
