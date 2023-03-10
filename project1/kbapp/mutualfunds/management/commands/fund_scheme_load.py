140107b0113e4b326e310077993bc461cfb38a01
Jishnu Kumar Srivastava <jisheras31@gmail.com>
2023-03-10 12:01:24 +0530

    Added schemes json structure

--- project1/kbapp/mutualfunds/management/commands/fund_scheme_load.py
+++ project1/kbapp/mutualfunds/management/commands/fund_scheme_load.py
@@ -22,6 +22,67 @@ import datetime
 #         {
 #             "scheme": "IS",
 
+# Now, we work on the schemes list.
+# The schemes list contains data of all the scehemes.
+# It looks like this:
+# "schemes": [
+#                     {
+#                         "code": null,
+#                         "fundname": "Bajaj MUTUAL FUND",
+#                         "schemeid": "ISQDD",
+#                         "scheme": "IS",
+#                         "schdesc": "Bajaj Regular Saver Fund",
+#                         "plan": "QD",
+#                         "plandesc": "Regular Quarterly IDCW",
+#                         "option": "D",
+#                         "optiondesc": "Payout",
+#                         "active": "Y",
+#                         "execdt": "2010-07-16T00:00:00.000Z",
+#                         "opendate": "2010-05-24T00:00:00.000Z",
+#                         "nav_sp": 4,
+#                         "desc": "Bajaj Regular Saver Fund Regular Quarterly IDCW Payout",
+#                         "prodcode": "XS71",
+#                         "nature": "Open",
+#                         "closedate": "2010-06-21T00:00:00.000Z",
+#                         "purcuttime": 1500,
+#                         "redcuttime": 1500,
+#                         "swicuttime": 1500,
+#                         "maturitydt": "9999-03-06T00:00:00.000Z",
+#                         "purallow": "Y",
+#                         "redallow": "Y",
+#                         "swiallow": "Y",
+#                         "swoallow": "Y",
+#                         "latiallow": "Y",
+#                         "latoallow": "Y",
+#                         "stpiallow": "Y",
+#                         "stpoallow": "Y",
+#                         "sipallow": "Y",
+#                         "swdallow": "Y",
+#                         "mcrid": "A3",
+#                         "sweepinallow": "Y",
+#                         "sweepoutallow": "Y",
+#                         "allotmentdt": "2010-07-16T00:00:00.000Z",
+#                         "isin": "INF846K01701",
+#                         "shortdesc": "AXF REG SAVER FUND  DIVD(QLY)",
+#                         "nfoallow": null,
+#                         "facevalue": 10,
+#                         "amficode": "112925",
+#                         "reopendate": "2010-05-24T00:00:00.000Z",
+#                         "nfoidentifier": "",
+#                         "new_minamt": 5000,
+#                         "add_minamt": null,
+#                         "red_minamt": 0,
+#                         "sip_minamt": null,
+#                         "swp_minamt": 1000,
+#                         "stp_minamt": 1000,
+#                         "switch_minamt": 0
+#                     },
+#                     {
+
+# and then other schemes.
+# This goes on till the end of the list. After that we have the next fund. So, we need to iterate over the funds list and then the schemes list.
+
+
 def get_data_from_api(url):
     response = requests.get(url)
     data = response.json()
